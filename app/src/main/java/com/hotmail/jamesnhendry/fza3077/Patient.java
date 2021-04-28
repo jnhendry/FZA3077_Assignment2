f8743c86c8e27986dd30353d4b912c6d21e8ca84
jhen0013 <66223799+jhen0013@users.noreply.github.com>
2021-04-28 16:14:03 +0200

    sss

--- app/src/main/java/com/hotmail/jamesnhendry/fza3077/Patient.java
+++ app/src/main/java/com/hotmail/jamesnhendry/fza3077/Patient.java
@@ -2,4 +2,5 @@ package com.hotmail.jamesnhendry.fza3077;
 
 public class Patient {
     private String clinitianUsername,name,username,password,medicalrecordId;
+    private int mofo,mifi,lofi,beats,bro;
 }
