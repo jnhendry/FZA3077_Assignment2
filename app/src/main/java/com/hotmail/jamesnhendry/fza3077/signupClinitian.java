abe29136981c49c75a26724e2fbffe2ae3ce6239
jhen0013 <66223799+jhen0013@users.noreply.github.com>
2021-05-13 13:21:50 +0200

    added fragment transactions

--- app/src/main/java/com/hotmail/jamesnhendry/fza3077/signupClinitian.java
+++ app/src/main/java/com/hotmail/jamesnhendry/fza3077/signupClinitian.java
@@ -2,6 +2,8 @@ package com.hotmail.jamesnhendry.fza3077;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -60,5 +62,10 @@ public class signupClinitian extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_signup_clinitian, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
--- app/src/main/java/com/hotmail/jamesnhendry/fza3077/signupPatient.java
+++ app/src/main/java/com/hotmail/jamesnhendry/fza3077/signupPatient.java
@@ -2,6 +2,8 @@ package com.hotmail.jamesnhendry.fza3077;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -60,5 +62,13 @@ public class signupPatient extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_signup_patient, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
     }
 }
