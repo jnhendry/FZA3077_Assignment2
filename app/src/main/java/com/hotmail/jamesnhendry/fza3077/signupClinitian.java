9c5ffde788d9676619e6335fed2af7a3df79d13c
jhen0013 <66223799+jhen0013@users.noreply.github.com>
2021-05-13 21:53:07 +0200

    clinitian

--- app/src/main/java/com/hotmail/jamesnhendry/fza3077/signupClinitian.java
+++ app/src/main/java/com/hotmail/jamesnhendry/fza3077/signupClinitian.java
@@ -13,6 +13,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,14 +35,16 @@ import android.widget.TextView;
 public class signupClinitian extends Fragment {
 
 
-    private EditText firstName;
-    private EditText surname;
-    private Spinner cSpecialisation;
-    private EditText email;
-    private EditText password;
-    private EditText retypePassword;
-    private Button signUpButton;
-    private TextView switchToLogIn;
+    private EditText edtfirstName;
+    private EditText edtsurname;
+    private Spinner spncSpecialisation;
+    private EditText edtemail;
+    private EditText edtpassword;
+    private EditText edtretypePassword;
+    private Button btnsignUpButton;
+    private TextView txtswitchToLogIn;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -82,13 +97,58 @@ public class signupClinitian extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        firstName = view.findViewById(R.id.edtClinitianNameSU);
-        surname = view.findViewById(R.id.edtClinitianSurnamesu);
-        cSpecialisation = view.findViewById(R.id.spnClinitianSpeciality);
-        email = view.findViewById(R.id.edtEmailClinitianSU);
-        password = view.findViewById(R.id.edtClinitianPasswordSU);
-        retypePassword = view.findViewById(R.id.edtPasswordresu);
-        signUpButton = view.findViewById(R.id.btnSignUpClinitian);
-        switchToLogIn = view.findViewById((R.id.txtSignInClinitianSU));
+        edtfirstName = view.findViewById(R.id.edtClinitianNameSU);
+        edtsurname = view.findViewById(R.id.edtClinitianSurnamesu);
+        spncSpecialisation = view.findViewById(R.id.spnClinitianSpeciality);
+        edtemail = view.findViewById(R.id.edtEmailClinitianSU);
+        edtpassword = view.findViewById(R.id.edtClinitianPasswordSU);
+        edtretypePassword = view.findViewById(R.id.edtPasswordresu);
+        btnsignUpButton = view.findViewById(R.id.btnSignUpClinitian);
+        txtswitchToLogIn = view.findViewById((R.id.txtSignInClinitianSU));
+        db = FirebaseFirestore.getInstance();
+
+        String email,password;
+        email = edtemail.getText().toString();
+        password = edtpassword.getText().toString();
+        signUp(email,password);
+
+    }
+
+    private void signUp(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    String name,specialisation,email,userID;
+
+                    name = edtfirstName.getText().toString()+ " " + edtsurname.getText().toString();
+                    specialisation = spncSpecialisation.getSelectedItem().toString();
+                    email = user.getEmail();
+                    userID = user.getUid();
+
+                    Map<String,Object> doc = new HashMap<>();
+                    doc.put("name",name);
+                    doc.put("specialisation",specialisation);
+                    doc.put("email",email);
+
+                    db.collection("Clinitian").document(userID).set(doc).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(getContext(), "Yay doc", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getContext(), "Boo", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                }else{
+                    Toast.makeText(getContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
