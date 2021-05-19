b1c47ed16428b01d30cd41dfcc1c8e7ed83b83cb
jhen0013 <66223799+jhen0013@users.noreply.github.com>
2021-05-20 00:51:25 +0200

    PDF WORKING YAH!!!!!!

--- app/src/main/java/com/hotmail/jamesnhendry/fza3077/NewVisit.java
+++ app/src/main/java/com/hotmail/jamesnhendry/fza3077/NewVisit.java
@@ -2,12 +2,23 @@ package com.hotmail.jamesnhendry.fza3077;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,9 +32,15 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class NewVisit extends AppCompatActivity {
 
@@ -36,10 +53,12 @@ public class NewVisit extends AppCompatActivity {
     private ArrayList<Note> noteArrayList = new ArrayList<>();
     private ArrayList<Recommendation> recommendationArrayList = new ArrayList<>();
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private Button btnUpdate,btnSimulate;
+    private Button btnUpdate,btnSimulate,btnGeneratePDF;
     String usertype,futureorpast;
     String visitID;
     private MedicalRecord medrec;
+    private static final int PERMISSION_REQUEST_CODE = 200;
+
 
 
     @Override
@@ -68,6 +87,12 @@ public class NewVisit extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 createnewRecommendation();
+            }
+        });
+        btnGeneratePDF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                generatePDF();
             }
         });
         btnSimulate.setOnClickListener(new View.OnClickListener() {
@@ -127,6 +152,7 @@ public class NewVisit extends AppCompatActivity {
                     btnSaveVisit.setVisibility(View.GONE);
                     btnaddNote.setVisibility(View.GONE);
                     btnaddRecommendation.setVisibility(View.GONE);
+                    btnGeneratePDF.setVisibility(View.VISIBLE);
                     updateviewforcompleted();
                     return;
                 case "clinician":
@@ -136,7 +162,8 @@ public class NewVisit extends AppCompatActivity {
                        btnSimulate.setVisibility(View.GONE);
                        updateviewforcompleted();
                        makeViewUneditable();
-                       return;
+                        btnGeneratePDF.setVisibility(View.VISIBLE);
+                        return;
                     }else {
                         btnSaveVisit.setVisibility(View.VISIBLE);
                         btnUpdate.setVisibility(View.GONE);
@@ -171,6 +198,180 @@ public class NewVisit extends AppCompatActivity {
         setuprecyclers();
     }
 
+    private void generatePDF() {
+        int pageHeight = 1120;
+        int pagewidth = 792;
+        Bitmap bmp, scaledbmp;
+        bmp = BitmapFactory.decodeResource(getResources(), R.drawable.gfgimage);
+        scaledbmp = Bitmap.createScaledBitmap(bmp, 280, 280, false);
+        if (checkPermission()) {
+            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+        } else {
+            requestPermission();
+        }
+
+        PdfDocument pdfDocument = new PdfDocument();
+        Paint paint = new Paint();
+        Paint title = new Paint();
+
+        PdfDocument.PageInfo mypageInfo = new PdfDocument.PageInfo.Builder(pagewidth, pageHeight, 1).create();
+        PdfDocument.Page myPage = pdfDocument.startPage(mypageInfo);
+        Canvas canvas = myPage.getCanvas();
+        canvas.drawBitmap(scaledbmp, 56, 40, paint);
+
+        title.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL));
+
+        title.setTextSize(15);
+        title.setColor(ContextCompat.getColor(this, R.color.black));
+        canvas.drawText("Document ID :  "+visitID,500,140,title);
+
+
+        title.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
+        title.setColor(ContextCompat.getColor(this, R.color.black));
+        title.setTextSize(15);
+
+        title.setTextAlign(Paint.Align.LEFT);
+
+        canvas.drawText("Clinitian : "+txtclinititanname.getText().toString(),100,350,title);
+
+        title.setTextSize(21);
+
+        canvas.drawText("Patient Details : ",100,380,title);
+        title.setTextSize(15);
+        canvas.drawText("Name :        "+txtpatientname.getText().toString(),120,420,title);
+        canvas.drawText("Age :            "+txtpatientage.getText().toString(),120,440,title);
+        canvas.drawText("Gender :      "+txtpatientGender.getText().toString(),120,460,title);
+        canvas.drawText("Location :    "+txtpatientlocation.getText().toString(),120,480,title);
+
+        title.setTextSize(21);
+
+
+        canvas.drawText("Lab Measurements : ",100,520,title);
+
+        title.setTextSize(15);
+
+        canvas.drawText("Blood pressure :                                        "+bloodpressure.getText().toString(),120,550,title);
+        canvas.drawText("C Reactive protein :                                   "+creactive.getText().toString(),120,570,title);
+        canvas.drawText("Apolipo protein A :                                    "+apolprotA.getText().toString(),120,590,title);
+        canvas.drawText("Apolipo protein B :                                    "+apolprotB.getText().toString(),120,610,title);
+        canvas.drawText("Lipoprotein A :                                           "+lipoprotA.getText().toString(),120,630,title);
+        canvas.drawText("Hemoglobin A1 :                                       "+hemoA.getText().toString(),120,650,title);
+        canvas.drawText("Is a smoker :                                              "+smoker.getSelectedItem().toString(),120,670,title);
+        canvas.drawText("Family History of heart disease :              "+famhist.getSelectedItem().toString(),120,690,title);
+        canvas.drawText("Reynolds risk score :                                  "+txtreynoldsriskscore.getText().toString(),120,710,title);
+
+        title.setTextSize(21);
+
+        canvas.drawText("Notes  :  ",100,760,title);
+        int counter = 760;
+        title.setTextSize(10);
+        for(int i = 0;i<noteArrayList.size();i++){
+            counter+=15;
+            canvas.drawText("Description : "+(noteArrayList.get(i).getDescription()),120,counter,title);
+            counter+=15;
+            canvas.drawText("Body :            "+(noteArrayList.get(i).getBody()),120,counter,title);
+            counter+=15;
+        }
+        title.setTextSize(21);
+        canvas.drawText("Recommendations  :  ",450,760,title);
+        int counter2 = 760;
+        title.setTextSize(10);
+        for(int i = 0;i<recommendationArrayList.size();i++){
+            counter2+=15;
+            canvas.drawText("Description : "+(recommendationArrayList.get(i).getDescription()),470,counter2,title);
+            counter2+=15;
+            canvas.drawText("Body :            "+(recommendationArrayList.get(i).getBody()),470,counter2,title);
+            counter2+=15;
+        }
+
+
+
+    //blood pressure
+        //c reactive Protein
+        //apolipo protein A
+        //apolipo Protein B
+        //lipoprotein A
+        //Hemoglobin A1
+        //smoker
+        //family history
+
+
+
+
+
+
+        pdfDocument.finishPage(myPage);
+        File[] files = getExternalFilesDirs(null);
+        String dir= "";
+
+        if(files == null) {
+        } else {
+            if(files.length > 0) {
+                for(File file : files) {
+                    dir = file.getPath();
+
+                }
+            }
+        }
+            File parentDir = new File(dir + File.separator + getString(R.string.app_name) + File.separator + "PDF's");
+            if(!parentDir.exists()) {
+                parentDir.mkdirs();
+            }
+            String filename =  visitID + ".pdf";
+
+
+            File file = new File(parentDir , filename);
+
+            try {
+                // after creating a file name we will
+                // write our PDF file to that location.
+                pdfDocument.writeTo(new FileOutputStream(file));
+
+                // below line is to print toast message
+                // on completion of PDF generation.
+                Toast.makeText(NewVisit.this, "PDF file generated succesfully.", Toast.LENGTH_SHORT).show();
+            } catch(IOException e) {
+                // below line is used
+                // to handle error
+                e.printStackTrace();
+            }
+            // after storing our pdf to that
+            // location we are closing our PDF file.
+            pdfDocument.close();
+
+
+        }
+
+
+
+    private boolean checkPermission() {
+        // checking of permissions.
+        int permission1 = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+        int permission2 = ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE);
+        return permission1 == PackageManager.PERMISSION_GRANTED && permission2 == PackageManager.PERMISSION_GRANTED;
+    }
+    private void requestPermission() {
+        // requesting permissions if not provided.
+        ActivityCompat.requestPermissions(this, new String[]{WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+    }
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0) {
+
+                // after requesting permissions we are showing
+                // users a toast message of permission granted.
+                boolean writeStorage = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                boolean readStorage = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                if (writeStorage && readStorage) {
+                    Toast.makeText(this, "Permission Granted..", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Permission Denined.", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        }
+    }
 
 
     private void updateviewforcompleted() {
@@ -289,6 +490,7 @@ public class NewVisit extends AppCompatActivity {
         btnSaveVisit = findViewById(R.id.btnSaveVisit);
         btnUpdate = findViewById(R.id.btnUpdate);
         btnSimulate = findViewById(R.id.btnSimulate);
+        btnGeneratePDF = findViewById(R.id.btnGeneratePdf);
     }
 
 
@@ -398,6 +600,7 @@ public class NewVisit extends AppCompatActivity {
 
             updateVisitMedicalRecord(visitID, medrec);
             updatenotesandrecommendations(noteArrayList,recommendationArrayList);
+            btnGeneratePDF.setVisibility(View.VISIBLE);
             // newvisit.put("")
         }
     }
